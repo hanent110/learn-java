d12ea8b82a14a92e4d1e8b7611dfe12a531ae12b
hanent <hanent110@gmail.com>
2020-04-22 22:42:46 +0900

    오브젝트 형식의 기본타입 Null Casting 테스트

--- learn-java/src/main/java/com/dtreesoft/grammer/casting/PrimitiveCasting.java
+++ learn-java/src/main/java/com/dtreesoft/grammer/casting/PrimitiveCasting.java
@@ -3,7 +3,7 @@ package com.dtreesoft.grammer.casting;
 public class PrimitiveCasting {
     public static void main(String[] args) {
         PrimitiveCasting test = new PrimitiveCasting();
-        test.runTest();
+        test.nullCasting();
     }
 
     private void runTest() {
@@ -20,15 +20,31 @@ public class PrimitiveCasting {
         funcIntParam(12345);
 
         funcPrimitiveLongParam(12345678);   // long 형으로 형변환은 가능하지만.
-
-//        funcLongParam(5678);             // Long 형으로 형변환은 불가능.
-
-        funcLongParam(Long.valueOf(String.valueOf(5678)));
     }
 
-    private void funcLongParam(Long i) {
-//        int intA = (Integer) i;   // Integer 형을 Long 형으로 캐스팅 불가.
-//        int intB = (int) i;       // int 형을 Long 형으로 캐스팅 불가.
+    private void primitiveObjectCasting() {
+        int primitiveInt = 10;
+        Integer objectInt = 20;
+        long primitiveLong = 30L;
+        Long objectLong = 40L;
+
+        primitiveInt = (int) primitiveLong;         // long -> int 가능
+//        primitiveInt = (int) objectLong;          // Long -> int 불가능
+        primitiveLong = (long) primitiveInt;        // int -> long 가능
+        primitiveLong = (long) objectInt;           // Integer -> long 가능
+    }
+
+    private void stringCasting() {
+        long primitiveLong1 = Long.valueOf(String.valueOf(5678));
+        long primitiveLong2 = Long.parseLong(String.valueOf(5678));
+    }
+
+    private void nullCasting() {
+        Integer objectInt = null;
+        int primitiveInt = 0;
+
+//        primitiveInt = objectInt;     // EXCEPTION: 당연히 NullPointerException 발생
+        System.out.println(objectInt);  // print > "null"
     }
 
     private void funcPrimitiveLongParam(long pA) {
