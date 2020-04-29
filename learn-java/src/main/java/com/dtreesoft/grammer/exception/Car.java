73b07b96e76129865966f28321343ba5109c4ba4
hanent <hanent110@gmail.com>
2020-04-29 16:57:59 +0900

    try catch return 테스트.

--- learn-java/src/main/java/com/dtreesoft/grammer/exception/Car.java
+++ learn-java/src/main/java/com/dtreesoft/grammer/exception/Car.java
@@ -1,13 +1,14 @@
 package com.dtreesoft.grammer.exception;
 
 public class Car {
+    private final static boolean IS_THROW_EXCEPTION = true;
+
     public Board createBoard() {
         Board board = new Board();
-
         try {
             board.setSpeed(10);
 
-            if (false) {
+            if (IS_THROW_EXCEPTION) {
                 throw new Exception();
             }
 
@@ -28,5 +29,55 @@ public class Car {
         System.out.println("createBoard() after finally before return");
 
         return new Board("After Finally Created Object");
+    }
+
+    public int finallyJustReturnTest() {
+        // 실제 primitive type 의 값을 리턴 할 경우
+        // Exception 발생 유무와 상관없이 finally 에서 리턴된 값이 적용된다.
+
+        try {
+            if (IS_THROW_EXCEPTION) {
+                throw new Exception();
+            }
+
+            System.out.println("finallyReturnTest() return 1 at try");
+            return 1;
+
+        } catch (Exception e) {
+
+            System.out.println("finallyReturnTest() return 2 at catch");
+            return 2;
+
+        } finally {
+
+            System.out.println("finallyReturnTest() return 3 at finally");
+            return 3;
+        }
+    }
+
+    public int finallyLocalVariableReturnTest() {
+        // primitive type 의 로컬 변수에 값을 지정하고 catch 에서 그 변수를 리턴한 후
+        // finally 에서 로컬 변수값을 변경했을 경우
+        // try 혹은 catch 에서 return 할때의 값이 적용된다.
+
+        int retValue = 1;
+
+        try {
+            if (IS_THROW_EXCEPTION) {
+                throw new Exception();
+            }
+            retValue = 1;
+            System.out.println("finallyReturnTest() return 1 at try");
+            return retValue;
+
+        } catch (Exception e) {
+            retValue = 2;
+            System.out.println("finallyReturnTest() return 2 at catch");
+            return retValue;
+
+        } finally {
+            retValue = 3;
+            System.out.println("finallyReturnTest() return 3 at finally");
+        }
     }
 }
--- learn-java/src/main/java/com/dtreesoft/grammer/exception/ExceptionTest.java
+++ learn-java/src/main/java/com/dtreesoft/grammer/exception/ExceptionTest.java
@@ -3,8 +3,9 @@ package com.dtreesoft.grammer.exception;
 public class ExceptionTest {
     public static void main(String[] args) {
         ExceptionTest exceptionTest = new ExceptionTest();
-
         exceptionTest.finallyTest();
+        exceptionTest.finallyJustReturnTest();
+        exceptionTest.finallyLocalVariableReturnTest();
     }
 
     public void finallyTest() {
@@ -12,7 +13,22 @@ public class ExceptionTest {
         Board board = newCar.createBoard();
 
         System.out.println("board = " + board);
+        System.out.println("------------------------ finallyTest() end");
+    }
 
-        System.out.println("finallyTest() end");
+    public void finallyJustReturnTest() {
+        Car newCar = new Car();
+        int retValue = newCar.finallyJustReturnTest();
+
+        System.out.println("return value = " + retValue);
+        System.out.println("------------------------ finallyReturnTest() end");
+    }
+
+    public void finallyLocalVariableReturnTest() {
+        Car newCar = new Car();
+        int retValue = newCar.finallyLocalVariableReturnTest();
+
+        System.out.println("return value = " + retValue);
+        System.out.println("------------------------ finallyLocalVariableReturnTest() end");
     }
 }
