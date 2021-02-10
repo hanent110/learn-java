63cf5b4a5d90971cee1a67d028f618b0898a718f
hanent <hanent110@gmail.com>
2021-02-10 16:11:53 +0900

    LocalDateTime Test

--- learn-java/src/main/java/com/dtreesoft/grammer/date/LocalDateTimeTest.java
+++ learn-java/src/main/java/com/dtreesoft/grammer/date/LocalDateTimeTest.java
@@ -1,6 +1,11 @@
 package com.dtreesoft.grammer.date;
 
+import com.sun.javafx.binding.StringFormatter;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 public class LocalDateTimeTest {
     public static void main(String[] args) {
@@ -15,5 +20,41 @@ public class LocalDateTimeTest {
 
         LocalDateTime convNow = LocalDateTime.parse(stringNow);
         System.out.println(convNow.toString());
+
+        minusTest();
+
+        localDateTest();
+    }
+
+    private void localDateTest() {
+        LocalDate today = LocalDate.now();
+        System.out.println("localDateTest today: " + today.toString());     // 2021-01-20
+
+        String todayString = today.toString();
+
+        LocalDate parseDate = LocalDate.parse(todayString);
+        System.out.println("localDateTest parse: " + parseDate);
+
+        try {
+            LocalDate invalidParseDate = LocalDate.parse("todayString");
+            System.out.println("localDateTest invalidParseDate: " + invalidParseDate);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        boolean equalResult = today.isEqual(parseDate);
+        System.out.println("localDateTest equalResult: " + equalResult);
+    }
+
+    private void minusTest() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime minusDate = now.minusDays(7);
+
+        System.out.println(String.format("minusTest, now: {%s}, minusDate: {%s}", now, minusDate));
+    }
+
+    private void dateToLocalDateTime() {
+        Date tempDate = new Date();
+        LocalDateTime lastDate = new Timestamp(tempDate.getTime()).toLocalDateTime();
     }
 }
