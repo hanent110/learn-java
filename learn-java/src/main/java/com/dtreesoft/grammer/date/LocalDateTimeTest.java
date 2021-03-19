6cb7cfea9df1d560cb228a892acee09940ab45f4
hanent <hanent110@gmail.com>
2021-03-19 16:41:08 +0900

    MOD LocalDateTime Test

--- learn-java/src/main/java/com/dtreesoft/grammer/date/LocalDateTimeTest.java
+++ learn-java/src/main/java/com/dtreesoft/grammer/date/LocalDateTimeTest.java
@@ -1,42 +1,43 @@
 package com.dtreesoft.grammer.date;
 
-import com.sun.javafx.binding.StringFormatter;
-
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class LocalDateTimeTest {
+
     public static void main(String[] args) {
         LocalDateTimeTest localDateTimeTest = new LocalDateTimeTest();
         localDateTimeTest.mainTest();
+        localDateTimeTest.minusTest();
+        localDateTimeTest.localDateTest();
+        localDateTimeTest.epochTest();
     }
 
     private void mainTest() {
         LocalDateTime now = LocalDateTime.now();
         String stringNow = now.toString();
-        System.out.println(stringNow);
+        System.out.println(stringNow);  // 2021-03-18T11:00:24.785
 
         LocalDateTime convNow = LocalDateTime.parse(stringNow);
         System.out.println(convNow.toString());
-
-        minusTest();
-
-        localDateTest();
     }
 
     private void localDateTest() {
         LocalDate today = LocalDate.now();
-        System.out.println("localDateTest today: " + today.toString());     // 2021-01-20
+        System.out.println("localDateTest today: " + today.toString());     // 2021-03-18
 
         String todayString = today.toString();
 
         LocalDate parseDate = LocalDate.parse(todayString);
-        System.out.println("localDateTest parse: " + parseDate);
+        System.out.println("localDateTest parse: " + parseDate);            // 2021-03-18
 
         try {
-            LocalDate invalidParseDate = LocalDate.parse("todayString");
+            LocalDate invalidParseDate = LocalDate.parse("todayString"); // DateTimeParseException 발생.
             System.out.println("localDateTest invalidParseDate: " + invalidParseDate);
         } catch (Exception e) {
             System.out.println(e);
@@ -57,4 +58,25 @@ public class LocalDateTimeTest {
         Date tempDate = new Date();
         LocalDateTime lastDate = new Timestamp(tempDate.getTime()).toLocalDateTime();
     }
+
+    private void epochTest() {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("now = " + now);
+
+        long epochMilli = now.toInstant(ZoneOffset.of("+09:00")).toEpochMilli();
+        System.out.println("epochMilli = " + epochMilli);
+
+        LocalDateTime time1 = LocalDateTime.of(2021, 3, 18, 11, 10, 00);
+        LocalDateTime time2 = LocalDateTime.of(2021, 3, 18, 11, 11, 00);
+
+        long between = ChronoUnit.MILLIS.between(time1, time2);
+        System.out.println("between = " + between);
+
+        long between2 = ChronoUnit.MILLIS.between(time2, time1);
+        System.out.println("between2 = " + between2);
+
+        long instantNow = Instant.now().toEpochMilli();
+        System.out.println("instantNow = " + instantNow);
+    }
+
 }
